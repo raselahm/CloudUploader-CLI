4fb1e5e711a902371cbbc0e8e8bd0c27ba23424b
Rasel Ahmed <32113495+raselahm@users.noreply.github.com>
2023-11-10 02:14:56 -0500

    Connected to AWS S3 and added code to successfully upload or show upload

--- clouduploader.sh
+++ clouduploader.sh
@@ -17,4 +17,18 @@ else
   echo "File found: $FILE_PATH"
 fi
 
+if [ -f "$FILE_PATH" ]; then
+  echo "File found: $FILE_PATH"
+  echo "Uploading to S3 bucket..."
+  aws s3 cp "$FILE_PATH" s3://raselscloudcliuploaderbucket/
+  if [ $? -eq 0 ]; then
+    echo "Upload successful."
+  else
+    echo "Upload failed."
+    exit 1
+  fi
+else
+  echo "Error: File does not exist at path '$FILE_PATH'."
+  exit 1
+fi
 
